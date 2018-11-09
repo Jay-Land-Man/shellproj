aac525bc4f006d51b5c330b976601e22ec83b5cb
Jay-Land-Man <38345187+Jay-Land-Man@users.noreply.github.com>
2018-11-09 17:30:14 +0200

    excluding the react log

--- inst/dev/run_dev.R
+++ inst/dev/run_dev.R
@@ -5,6 +5,6 @@ rm(list = ls(all.names = TRUE))# clean environneent
 devtools::document('.') # create NAMESPACE and man
 devtools::load_all('.') # load package
 options(app.prod = FALSE) # TRUE = production mode, FALSE = development mode
-options(shinyreactlog = TRUE) # Press ctrl-F3 to view the reactivity diagram, could also try shiny::showReactLog()
+# options(shinyreactlog = TRUE) # Press ctrl-F3 to view the reactivity diagram, could also try shiny::showReactLog()
 shiny::runApp('inst/apps',
               launch.browser = TRUE) # run the main app
